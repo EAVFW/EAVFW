b0eccac78844d8a1b4c645e93a9ca382bf266b51
Poul Kjeldager Sørensen <pks@delegate.dk>
2021-12-27 16:24:44 +0100

    fixed export

--- packages/apps/src/index.ts
+++ packages/apps/src/index.ts
@@ -3,3 +3,5 @@ export * from "./EAVApp";
 export * from "./useModelDrivenApp";
 export * from "./ModelDrivenApp";
 export * from "./FormsConfig";
+export * from "./ModelDrivenSitemap";
+export * from "./ModelDrivenSitemapEntity";
