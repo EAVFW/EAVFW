1bcda8dfd0db16e188abf1ab61a83f25f02c97e9
Poul Kjeldager Sørensen <pks@delegate.dk>
2022-01-06 05:48:50 +0100

    added navigation

---
+++ packages/apps/src/Components/Navigation/ModelDrivenNavigation.tsx
@@ -0,0 +1,292 @@
+import {
+    Dropdown,
+    Icon,
+    IDropdownOption,
+    IDropdownStyles,
+    IButtonStyles,
+    PersonaInitialsColor,
+    PersonaSize,
+    Stack,
+    StackItem,
+    Link as FluentLink,
+    mergeStyles,
+    ITheme,
+    Persona,
+    ThemeContext,
+    Theme,
+    IStyleFunction,
+    IDropdownStyleProps,
+    ThemeProvider,
+    CommandButton,
+    IIconProps
+} from "@fluentui/react";
+import Link from "next/link";
+import React, { useEffect, useState } from 'react';
+import styles from "./ModelDrivenNavigation.module.scss";
+import { NextRouter, useRouter, } from 'next/router' 
+import { ModelDrivenSitemap } from "../../ModelDrivenSitemap";
+import { ModelDrivenSitemapEntity } from "../../ModelDrivenSitemapEntity";
+import { useUserProfile } from "../Profile/useUserProfile";
+import { useAppInfo } from "../../useAppInfo";
+import { useModelDrivenApp } from "../../useModelDrivenApp";
+import { ResolveFeature } from "../../FeatureFlags";
+
+
+interface WithRouterProps {
+    router: NextRouter
+}
+
+export interface ModelDrivenNavigationProps /*extends WithRouterProps, WithAppProps, WithUserProps*/ {
+    sitemap: ModelDrivenSitemap
+    theme?: Theme
+}
+
+export interface ModelDrivenNavigationState {
+    sitemap: ModelDrivenSitemap,
+    areas: IDropdownOption[],
+}
+
+const ModelDrivenGroupItemMarker = (t?: ITheme) => mergeStyles({
+    backgroundColor: t?.palette.themePrimary,
+    transition: "opacity 200ms ease 0s",
+    "opacity": 0,
+    borderRadius: 22,
+    borderRight: `2px solid ${t?.palette.themePrimary};`,
+    borderLeft: `2px solid ${t?.palette.themePrimary};`,
+    marginRight: 6.5,
+    height: "auto",
+    display: "inline-block",
+    selectors: {
+        '&.active': {
+            "opacity": 1,
+        }
+    }
+});
+
+const dropdownStyles: IStyleFunction<IDropdownStyleProps, IDropdownStyles> = props => ({
+    root: {
+        width: "100%"
+    },
+    title: {
+        border: 0, selectors: {
+            ':hover': {
+                backgroundColor: props.theme?.palette.neutralTertiaryAlt
+            }
+        }
+    },
+});
+
+const iconStyles: IButtonStyles = {
+    root: {
+        padding: 2,
+        margin: 2,
+        width: 40,
+
+    },
+    flexContainer: {
+        justifyContent: "center"
+    }
+};
+
+const IconLeft: IIconProps = {
+    iconName: 'DoubleChevronLeft',
+    style: { color: "#d0d0d0", fontSize: 12 }
+};
+
+const IconRight: IIconProps = {
+    iconName: 'DoubleChevronRight',
+    style: { color: "#d0d0d0", fontSize: 12 }
+};
+
+function filterEntry(user: any) {
+    return ([key, entry]: [string, ModelDrivenSitemapEntity]) => {
+        console.log("filter entry");
+        console.log([entry, user]);
+
+        if (!entry.roles)
+            return true;
+
+        return entry.roles.allowed?.filter(r => user.role.filter((rr: string) => r === rr).length > 0)?.length ?? 0 > 0;
+    };
+}
+
+export default function ModelDrivenNavigation(props: ModelDrivenNavigationProps) {
+
+    const app = useModelDrivenApp();
+    const appInfo = useAppInfo();
+    const router = useRouter();
+    const [areas, setAreas] = useState<ModelDrivenNavigationState["areas"]>([]);
+    const [sitemap, setSiteMap] = useState<ModelDrivenNavigationState["sitemap"]>(props.sitemap);
+    const [selectedArea, setselectedArea] = useState<string>();
+    const user = useUserProfile();
+    const [minimized, setMinized] = useState(true);
+
+    const appName = appInfo.currentAppName;//this.props.router.query.appname;
+
+    useEffect(() => {
+        console.groupCollapsed("AreaFilters");
+        try {
+            const areas = Object.keys(sitemap.areas).filter(area => {
+                console.log("area to filter");
+                console.log(sitemap.areas[area]);
+                console.log(user);
+
+                if (!user)
+                    return true;
+
+                let noRoleInfoDefined = true;
+                for (let g of Object.keys(sitemap.areas[area])) {
+                    let group = sitemap.areas[area][g];
+                    for (let e of Object.keys(group)) {
+                        let roles = group[e].roles;
+
+                        console.log(roles);
+                        if (roles) {
+                            noRoleInfoDefined = false;
+                            if (roles?.allowed?.filter(role => user.role.filter((r: string) => role === r).length > 0)?.length ?? 0 > 0) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+
+                return noRoleInfoDefined;
+            }).map(area => ({ key: area, text: area, id: area } as IDropdownOption));
+            setAreas(areas);
+
+            setselectedArea(router.query.area as string ?? areas[0]?.key);
+        } finally {
+            console.groupEnd();
+        }
+    }, [user])
+
+    const _areaChanged = (value: React.FormEvent<HTMLDivElement>, option?: IDropdownOption, index?: number) => {
+        console.log(arguments);
+        const selectedArea = option?.key as string;
+        setselectedArea(selectedArea);
+        // this.setState({ selectedArea: option?.key as string });
+        router.push(`/apps/${router.query.appname}/areas/${selectedArea}/`);
+    }
+
+    if (!user)
+        return <div>loading</div>
+
+    return (
+        <ThemeProvider id="themeNav" theme={props.theme ?? ResolveFeature("defaultTheme")}>
+            <ThemeContext.Consumer>
+                {(theme: Theme | undefined) =>
+                    <Stack verticalFill as="nav" className={`${!minimized ? styles.ModelDrivenNavigationSmall : styles.ModelDrivenNavigation}`}>
+                        <StackItem grow className={`${styles.ModelDrivenItemColumn}`}>
+                            {app.getConfig("SVG_LOGO_PATH") ? <img style={{ overflow: "visible" }} src={app.getConfig("SVG_LOGO_PATH")} /> :
+                                <img style={{ padding: 8, boxSizing: "border-box" }}
+                                    src="/logo.png"
+                                    alt="Logo"
+                                />
+                            }
+                            {selectedArea && <Stack as="ul" className={`${styles.ModelDrivenItemList}`}>
+
+                                {Object.entries(sitemap.areas[selectedArea])
+                                    .map(([groupKey, groupEntry]) =>
+                                        minimized ? (
+                                            <Stack as="li" key={groupKey}>
+                                                <span className={`${styles.ModelDrivenGroupHeader}`}>{groupKey}</span>
+                                                <Stack as="ul">
+                                                    {Object.entries(groupEntry).filter(filterEntry(user))
+                                                        .sort(([akey, aentry], [bkey, bentry]) => aentry.order - bentry.order)
+                                                        .map(([key, entry]) => (
+                                                            <li className={styles.ModelDrivenGroupItem} key={key + entry.logicalName}>
+                                                                <Stack horizontal>
+                                                                    <div
+                                                                        className={`${ModelDrivenGroupItemMarker(theme)} ${entry.logicalName === router.query.entityName && router.query.view === entry.viewName ? 'active' : ''}`}></div>
+                                                                    <Icon iconName="AddInIcon" />
+                                                                    {entry.viewName ?
+                                                                        <Link
+                                                                            href={`/apps/${appName}/areas/${selectedArea}/entities/${entry.logicalName}/views/${entry.viewName}`}>
+                                                                            <FluentLink
+                                                                                href={`/apps/${appName}/areas/${selectedArea}/entities/${entry.logicalName}/views/${entry.viewName}`}>    {entry.title}</FluentLink>
+                                                                        </Link>
+                                                                        :
+                                                                        <Link
+                                                                            href={`/apps/${appName}/areas/${selectedArea}/entities/${entry.logicalName}`}>
+                                                                            <FluentLink
+                                                                                href={`/apps/${appName}/areas/${selectedArea}/entities/${entry.logicalName}`}>    {entry.title}</FluentLink>
+                                                                        </Link>
+                                                                    }
+                                                                </Stack>
+                                                            </li>
+                                                        ))}
+                                                </Stack>
+                                            </Stack>
+                                        )
+                                            :
+                                            <Stack as="li" key={groupKey}>
+                                                <span className={`${styles.ModelDrivenGroupHeaderSmall}`}>
+                                                    <Persona
+                                                        imageInitials={groupKey.substring(0, 2).toUpperCase()}
+                                                        initialsColor={PersonaInitialsColor.warmGray}
+                                                        size={PersonaSize.size32}
+                                                    />
+                                                </span>
+                                                <Stack as="ul" style={{ justifyContent: "center" }}>
+                                                    {Object.entries(sitemap.areas[selectedArea][groupKey]).filter(filterEntry(user)).map(([key, entry]) => (
+                                                        <li className={styles.ModelDrivenGroupItemSmall} key={key + entry.logicalName}>
+                                                            <Stack horizontal>
+                                                                <div
+                                                                    className={`${ModelDrivenGroupItemMarker(theme)} ${entry.logicalName === router.query.entityName && router.query.view === entry.viewName ? 'active' : ''}`}></div>
+                                                                {entry.viewName ?
+                                                                    <div>
+                                                                        <Link
+                                                                            href={`/apps/${appName}/areas/${selectedArea}/entities/${entry.logicalName}/views/${entry.viewName}`}>
+                                                                            <FluentLink
+                                                                                href={`/apps/${appName}/areas/${selectedArea}/entities/${entry.logicalName}/views/${entry.viewName}`}>
+                                                                                <Persona
+                                                                                    imageInitials={entry.title.substring(0, 2).toUpperCase()}
+                                                                                    initialsColor={PersonaInitialsColor.blue}
+                                                                                    size={PersonaSize.size32}
+                                                                                />
+                                                                            </FluentLink>
+                                                                        </Link>
+                                                                    </div>
+                                                                    :
+                                                                    <div>
+                                                                        <Link
+                                                                            href={`/apps/${appName}/areas/${selectedArea}/entities/${entry.logicalName}`}>
+                                                                            <FluentLink
+                                                                                href={`/apps/${appName}/areas/${selectedArea}/entities/${entry.logicalName}`}>
+
+                                                                                <Persona
+                                                                                    imageInitials={entry.title.substring(0, 2).toUpperCase()}
+                                                                                    initialsColor={PersonaInitialsColor.blue}
+                                                                                    size={PersonaSize.size32}
+                                                                                />
+                                                                            </FluentLink>
+                                                                        </Link>
+                                                                    </div>
+                                                                }
+                                                            </Stack>
+
+                                                        </li>
+                                                    ))}
+                                                </Stack>
+                                            </Stack>
+                                    )}
+                            </Stack>
+                            }
+                        </StackItem>
+                        <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                            {minimized &&
+                                <Dropdown id="AreaSelector"
+                                    selectedKey={selectedArea} onChange={_areaChanged}
+                                    options={areas}
+                                    styles={dropdownStyles}
+                                />
+                            }
+                            <CommandButton styles={iconStyles} iconProps={minimized ? IconLeft : IconRight}
+                                onClick={() => setMinized(!minimized)} />
+                        </div>
+                    </Stack>
+                }
+            </ThemeContext.Consumer>
+        </ThemeProvider>
+    )
+}
---
+++ packages/apps/src/Components/TopBar/ModelDrivenTopBar.tsx
@@ -0,0 +1,122 @@
+import {
+    PersonaSize,
+    Stack,
+    Persona,
+    IStackProps,
+    ITheme,
+    Link as FluentLink,
+    Text,
+    useTheme,
+    ThemeProvider,
+    Theme,
+    Modal,
+    mergeStyleSets,
+} from "@fluentui/react";
+
+import Link from "next/link";
+import React, { useContext } from "react";
+import useSWR from "swr";
+import { useId, useBoolean } from '@fluentui/react-hooks';
+import { useUserProfile } from "../Profile";
+import { ResolveFeature } from "../../FeatureFlags";
+
+
+export type TopBarProps = {
+    title: string;
+    search: boolean;
+    theme?: Theme
+}
+
+const styles2: IStackProps["styles"] = (p: IStackProps, t: ITheme) => ({
+    root: {
+        height: 42,
+        paddingLeft: 8/* background: t.palette.themePrimary, color: t.palette.white*/
+    }
+});
+
+const personaStyles: IStackProps["styles"] = (p: IStackProps, t: ITheme) => ({
+    root: {
+        margin: 0, padding: 0,
+        cursor: "pointer",
+        ':hover': {
+            backgroundColor: t.palette.neutralLighterAlt,
+        },
+    }
+});
+
+
+const contentStyles = mergeStyleSets({
+    container: {
+        display: 'flex',
+        flexFlow: 'column nowrap',
+        alignItems: 'stretch',
+        position: "absolute",
+        top: 42,
+        right: 0,
+        width: 150,
+        padding: 8
+    },
+    persona: {}
+});
+
+export function TopBar(props: TopBarProps) {
+    const profile = useUserProfile();
+    console.log(profile);
+    const defaultTheme = ResolveFeature("defaultTheme");
+    const [isModalOpen, { setTrue: showModal, setFalse: hideModal, toggle }] = useBoolean(false);
+    const titleId = useId('title');
+    //   const user = useContext(UserContext);
+    return (
+        <ThemeProvider theme={props.theme ?? defaultTheme}>
+            <Stack horizontal horizontalAlign="space-between" verticalAlign="center" tokens={{ childrenGap: 10 }}
+                styles={styles2}>
+                <Stack horizontal horizontalAlign="start">
+                    <Link href="/">
+                        <FluentLink theme={useTheme()} href="/" title="Home" role="link"> <Text
+                            variant="large"> {props.title}</Text> </FluentLink>
+                    </Link>
+                </Stack>
+
+                <Stack horizontal horizontalAlign="center">
+
+                </Stack>
+                <Stack horizontal horizontalAlign="end" verticalAlign="center" styles={personaStyles}>
+                    {profile &&
+                        <>
+                            <Persona onClick={toggle}
+                            text={`${profile.name || profile.email}`}
+                            secondaryText={profile.email}
+                            imageInitials={((profile.name as string)?.trim().split(' ').map((n: any) => n[0].toUpperCase()).join('') || profile.email.split('@')[0])}
+                                size={PersonaSize.size32}
+                                hidePersonaDetails={!true}
+                            imageAlt={`${profile.name || profile.email}`}
+                            />
+
+                            <ThemeProvider theme={defaultTheme}>
+                                <Modal
+                                    titleAriaId={titleId}
+                                    isOpen={isModalOpen}
+                                    onDismiss={hideModal}
+                                    isModeless={true}
+                                    containerClassName={contentStyles.container}
+                                    dragOptions={undefined}
+                                >
+                                    <Stack horizontal horizontalAlign="end" verticalAlign="center">
+                                        <Link href={`/.auth/logout?post_logout_redirect_uri=${location.href}`}>
+                                            <FluentLink href={`/.auth/logout?post_logout_redirect_uri=${location.href}`} title="Signout" role="link" > <Text variant="mediumPlus" >Log ud</Text> </FluentLink>
+                                        </Link>
+                                    </Stack>
+
+                                    <Stack horizontal horizontalAlign="start" verticalAlign="center">
+                                    <Text variant="mediumPlus">{profile.fullname}</Text>
+                                    {profile.role && profile.role.map((role: string) => <div key={role}><br /><Text variant="mediumPlus">{role}</Text></div>)}
+                                    </Stack>
+
+                                </Modal>
+                            </ThemeProvider>
+                        </>}
+                </Stack>
+            </Stack>
+        </ThemeProvider>
+    );
+}
