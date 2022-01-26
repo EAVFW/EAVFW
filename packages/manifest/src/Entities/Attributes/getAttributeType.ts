90f73f45c79b9d0898266f38a9f33feb5adb1e87
Poul Kjeldager Sørensen <pks@delegate.dk>
2022-01-26 09:13:01 +0100

    nullable type support

--- packages/manifest/src/Entities/Attributes/getAttributeType.ts
+++ packages/manifest/src/Entities/Attributes/getAttributeType.ts
@@ -2,5 +2,5 @@ import { AttributeDefinition } from "./AttributeDefinition";
 
 
 export function getAttributeType(attribute: AttributeDefinition) {
-    return typeof attribute.type === "string" ? attribute.type : attribute.type.type;
+    return typeof attribute.type === "string" ? attribute.type : attribute.type?.type;
 }
