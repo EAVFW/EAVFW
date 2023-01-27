55dded532a7ee257495f99d09d7f27e965304fe0
Poul Kjeldager Sørensen <pks@delegate.dk>
2023-01-27 10:39:10 +0100

    feat: added polylookup

--- packages/manifest/src/Entities/Attributes/Types/Lookup/isLookup.ts
+++ packages/manifest/src/Entities/Attributes/Types/Lookup/isLookup.ts
@@ -1,6 +1,11 @@
 import { AttributeTypeDefinition } from "../AttributeTypeDefinition";
-import { LookupType } from "./LookupType";
+import { LookupType, PolyLookupType } from "./LookupType";
 
 export function isLookup(type: AttributeTypeDefinition): type is LookupType {
-    return typeof (type) === "object" && type.type === "lookup";
+    return typeof (type) === "object" && (type.type?.toLowerCase() === "lookup" || type.type?.toLowerCase() === "polylookup");
+}
+
+
+export function isPolyLookup(type: AttributeTypeDefinition): type is PolyLookupType {
+    return typeof (type) === "object" && type.type?.toLowerCase() === "polylookup";
 }
