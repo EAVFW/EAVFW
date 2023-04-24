5ebf026088297a061ca067e79cf75d966e21af73
emlunde <42939315+emlunde@users.noreply.github.com>
2023-04-24 13:40:11 +0200

    mouse over for info description added instead of onClick

--- packages/apps/src/Components/Forms/AutoForm/Templates/FieldTemplate.tsx
+++ packages/apps/src/Components/Forms/AutoForm/Templates/FieldTemplate.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useMemo } from "react";
+import React, { createContext, useContext, useMemo, useState } from "react";
 import { FieldTemplateProps } from "@rjsf/core";
 import { Callout, FontWeights, getTheme, IButtonStyles, IconButton, IIconProps, IStackStyles, IStackTokens, ITheme, Label, mergeStyleSets, Stack, Text, ThemeContext } from "@fluentui/react";
 import { List } from "@fluentui/react";
@@ -98,6 +98,9 @@ export const EAVFWLabel: React.FC<{ id?: string, label: string, required?: boole
     const warnings = useContext(WarningContext);
     const [isWarningCalloutVisible, { toggle: toggleIsWarningCalloutVisible }] = useBoolean(false);
     const iconButtonWarningId = useId('iconButtonWarningId');
+    const [isInfoCalloutVisibleOnHover, setIsInfoCalloutVisibleOnHover] = useState(false)
+    const [isWarningCalloutVisibleOnHover, setIsWarningCalloutVisibleOnHover] = useState(false)
+
 
     return (
         <>
@@ -110,6 +113,8 @@ export const EAVFWLabel: React.FC<{ id?: string, label: string, required?: boole
                     title="Info"
                     ariaLabel="Info"
                     onClick={toggleIsCalloutVisible}
+                    onMouseEnter={() => setIsInfoCalloutVisibleOnHover(true)}
+                    onMouseLeave={() => setIsInfoCalloutVisibleOnHover(false)}
                     styles={iconButtonStyles}
                 />}
                 {warnings.length > 0 && <IconButton
@@ -122,7 +127,7 @@ export const EAVFWLabel: React.FC<{ id?: string, label: string, required?: boole
                 />}
             </Stack>
 
-            {isInfoCalloutVisible && (
+            {isInfoCalloutVisibleOnHover && (
                 <Callout
                     target={'#' + iconButtonId}
                     setInitialFocus
