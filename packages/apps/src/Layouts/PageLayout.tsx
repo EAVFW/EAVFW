a8cabed8ec4a4b2c2105a816ff2245998c0c9e25
Poul Kjeldager Sørensen <pks@delegate.dk>
2023-10-18 15:18:25 +0200

    removed poc code

--- packages/apps/src/Layouts/PageLayout.tsx
+++ packages/apps/src/Layouts/PageLayout.tsx
@@ -7,41 +7,13 @@ import { PageLayoutProps } from "./PageLayoutProps";
 import { PageStackStyles } from "./PageStackStyles";
 
  
-import {
-    DrawerBody,
-    DrawerHeader,
-    DrawerHeaderTitle,
-    Drawer,
-    DrawerProps,
-} from "@fluentui/react-components/unstable";
-import {
-    Button,
-    Label,
-    Radio,
-    RadioGroup,
-    makeStyles,
-    shorthands,
-    tokens,
-    useId,
-} from "@fluentui/react-components";
-import { Dismiss24Regular } from "@fluentui/react-icons";
-
-type DrawerType = Required<DrawerProps>["type"];
 
 export function PageLayout(props: PageLayoutProps) {
     console.log(props);
 
     const topBarTheme = ResolveFeature("topBarTheme");
 
-    const [isOpen, setIsOpen] = React.useState(false);
-    const [type, setType] = React.useState<DrawerType>("overlay");
-    useEffect(() => {
-        setTimeout(() => {
-            console.log("Opening drawer");
-            setIsOpen(true)
-        }, 10000);
-
-    }, [])
+   
     if (!props.sitemap)
         return <div>loading</div>
 
@@ -54,31 +26,7 @@ export function PageLayout(props: PageLayoutProps) {
                 <Stack styles={PageStackStyles} horizontal verticalFill>
                     <ModelDrivenNavigation sitemap={props.sitemap} theme={topBarTheme} />
                     <Stack.Item grow>
-                        <Drawer
-                            type={type}
-                            separator
-                            open={isOpen}
-                            onOpenChange={(_, { open }) => setIsOpen(open)}
-                        >
-                            <DrawerHeader>
-                                <DrawerHeaderTitle
-                                    action={
-                                        <Button
-                                            appearance="subtle"
-                                            aria-label="Close"
-                                            icon={<Dismiss24Regular />}
-                                            onClick={() => setIsOpen(false)}
-                                        />
-                                    }
-                                >
-                                    Default Drawer
-                                </DrawerHeaderTitle>
-                            </DrawerHeader>
-
-                            <DrawerBody>
-                                <p>Drawer content</p>
-                            </DrawerBody>
-                        </Drawer>
+                        
                         {props.children}
                     </Stack.Item>
                 </Stack>
