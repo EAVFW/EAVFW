c551b41cffabbbce7e8a3af91d4cfdaef7a04f9b
Lasse Gjetrup Eskildsen <lasse.g.eskildsen@gmail.com>
2023-11-02 13:46:25 +0100

    Update packages/forms/src/EAVFormContextActions.ts
    Co-authored-by: Poul Kjeldager Sørensen <poul@kjeldager.com>

--- packages/forms/src/EAVFormContextActions.ts
+++ packages/forms/src/EAVFormContextActions.ts
@@ -7,7 +7,7 @@ export type EAVFormOnChangeCallback<T> = (props: T, ctx: EAVFormOnChangeCallback
 export type EAVFOrmOnChangeHandler<T> = (cb: EAVFormOnChangeCallback<T>) => void
 
 export type EAVFormContextActions<T> = {    
-    runValidation: (complete?: () => void, manipulateResult?: (errors: EAVFWErrorDefinition) => EAVFWErrorDefinition) => void;
+    runValidation: (onComplete?: () => void, manipulateResult?: (errors: EAVFWErrorDefinition) => EAVFWErrorDefinition) => void;
     updateState: <TState extends EAVFormContextState<T>>(cb: (state: TState, ctx: { replaceState: boolean }) => void) => {changedProp: boolean, changedValues: any } | undefined | void;
     onChange: EAVFOrmOnChangeHandler<T>;
     addVisited: (id: string) => void;
