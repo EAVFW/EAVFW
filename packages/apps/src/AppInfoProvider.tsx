e812adfb76ad7fc5d7980f8ff800f346e9896386
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-02-01 12:00:12 +0100

    fix: fixed such not creating a new object per render loop

--- packages/apps/src/AppInfoProvider.tsx
+++ packages/apps/src/AppInfoProvider.tsx
@@ -1,9 +1,9 @@
-import { useContext, useMemo } from "react";
+import { PropsWithChildren, useContext, useMemo } from "react";
 import { AppInfoContext } from "./AppInfoContext";
 import { AppNavigationContext } from "./AppNavigationContext";
 import { useModelDrivenApp } from "./useModelDrivenApp";
 
-export const AppInfoProvider: React.FC = ({ children }) => {
+export const AppInfoProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
 
     const modelapp = useModelDrivenApp();
     const { currentAppName } = useContext(AppNavigationContext)!;
@@ -11,12 +11,12 @@ export const AppInfoProvider: React.FC = ({ children }) => {
     const title = useMemo(() => {
         const app = modelapp.getApp(currentAppName);
 
-        return app?.title ?? currentAppName ?? modelapp.getApps()[0][0] ?? "No Name";
+        return { title: app?.title ?? currentAppName ?? modelapp.getApps()[0][0] ?? "No Name" };
     }, [currentAppName]);
 
 
     return (
-        <AppInfoContext.Provider value={{ title }}>
+        <AppInfoContext.Provider value={title}>
             {children}
         </AppInfoContext.Provider>
     )
