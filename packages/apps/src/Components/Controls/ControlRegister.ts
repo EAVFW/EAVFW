84650e64bc99cce85effbad8b9ebc0f44bc1110f
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-02-12 11:36:19 +0100

    fix: fixed typo on registercontrol, deprecated registerecontrol.

--- packages/apps/src/Components/Controls/ControlRegister.ts
+++ packages/apps/src/Components/Controls/ControlRegister.ts
@@ -1,6 +1,17 @@
 
 export const Controls: { [key: string]: any } = {};
 
+
+/**
+ * Deprecated - will be removed in future update. Use RegisterControl instead.
+ * @param name
+ * @param control
+ */
 export function RegistereControl(name: string, control: any) {
+    Controls[name] = control;
+}   
+
+
+export function RegisterControl(name: string, control: any) {
     Controls[name] = control;
 }   
