0bb4b92ed70ee70ade86b515027eff8285ad93bf
Kasper Baun <kasperbaun@hotmail.com>
2024-05-21 17:26:55 +0200

    fix: dashboards back to how it was

--- packages/apps/src/ModelDrivenSitemap.ts
+++ packages/apps/src/ModelDrivenSitemap.ts
@@ -2,8 +2,15 @@ import { ModelDrivenSitemapEntry } from "./ModelDrivenSitemapEntry";
 
 export interface ModelDrivenSitemap {
     // dashboards is deprecated and should be removed
-    dashboards: {},
-    
+
+    dashboards: {
+        [area: string]: {
+            [dashboard: string]: {
+                url: string
+            }
+        }
+    },
+
     areas: {
         [key: string]: {
             [key: string]: {
