74f432b7ee3513c53a18ec71043c88330b187562
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-06-25 07:52:53 +0200

    fix: added types for the manifest app object

--- packages/manifest/src/ManifestDefinition.ts
+++ packages/manifest/src/ManifestDefinition.ts
@@ -2,11 +2,33 @@
 import { LocalizationDefinition } from "./Localization"
 import { DashboardDefinition, EntityDefinition } from "./Entities";
 
+export type ManifestRoleAccessDefinition = {
+    allowed?: string[]
+}
+
+export type ManifestAppSitemapDefinition = {
+    groups?: {
+        [key: string]: {
+            title: string,
+            locale?: { [locale: string]: {title:string } };
+        }
+    }
+}
+export type ManifestAppDefinition = {
+    title?: string,
+    roles?: ManifestRoleAccessDefinition,
+    sitemap?: ManifestAppSitemapDefinition,
+    [key: string]: any
+}
+export type ManifestAppsDefinition = {
+    
+    [key: string]: ManifestAppDefinition
+}
 export type ManifestDefinition = {
     localization?: { [locale: string]: LocalizationDefinition };
     config: any;
     variables: any;
-    apps: any;
+    apps: ManifestAppsDefinition;
     entities: { [entity: string]: EntityDefinition };
     dashboards?: { [dashboard: string]: DashboardDefinition };
     errorMessages?: { [locale: string]: { [code: string]: string } };
