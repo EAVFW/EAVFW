0adccf63dd6e8039ff11f57a1c3129f7c1ff594d
Poul Kjeldager Sørensen <pks@delegate.dk>
2024-06-25 07:50:00 +0200

    fix: made removeNonAlphanumeric also fix diacritics, and map æøå to ae, o,aa

--- packages/utils/src/removeNonAlphanumeric.tsx
+++ packages/utils/src/removeNonAlphanumeric.tsx
@@ -1,3 +1,5 @@
-export function removeNonAlphanumeric(inputString:string) {
-    return inputString.replace(/[^a-zA-Z0-9]/g, '');
-};
+﻿export function removeNonAlphanumeric(inputString:string) {
+    return inputString.normalize("NFD").replace(/[\u0300-\u036f]/g, '') // Removes diacritics
+        .replace(/æ/g, 'ae').replace(/ø/g, 'o').replace(/å/g, 'aa') // Specific replacements
+        .replace(/[^a-zA-Z0-9]/g, ''); // Removes non-alphanumeric characters
+}
